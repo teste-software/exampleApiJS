c81af0d8969201f2df8fdc02252a6ce089f9f411
teste-software <122368047+teste-software@users.noreply.github.com>
2023-07-03 10:38:25 -0300

    Update app-settings.js

--- app-settings.js
+++ app-settings.js
@@ -1,3 +1,4 @@
-
+const path = require('path');
 global.server.http_port = 3000;
 global.name = 'TESTE API JS'
+global.teste = path.resolve(process.env.PWD || '', "ssl/apple_push_notification/key.p8");
